00a431470483467e015acf35cdcb6a9dc1637a78
yangyang1 <yangyang02@bamboocloud.cn>
2025-11-08 23:39:21 +0800

    20251108-添加WebSocket长连接, 保证实时获取节点状态

--- src/main/java/com/lowcode/workflow/runner/graph/machine/NodeInstanceStateMachine.java
+++ src/main/java/com/lowcode/workflow/runner/graph/machine/NodeInstanceStateMachine.java
@@ -1,6 +1,8 @@
 package com.lowcode.workflow.runner.graph.machine;
 
 import com.lowcode.workflow.runner.graph.data.struct.instance.NodeInstance;
+import com.lowcode.workflow.runner.graph.websocket.handler.NodeStatusWebSocketHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -12,12 +14,18 @@ import java.time.LocalDateTime;
 @Component
 @com.lowcode.workflow.runner.graph.annotation.StateMachine(value = "node", type = NodeInstance.class)
 public class NodeInstanceStateMachine implements StateMachine<NodeInstance>{
+
+    @Autowired
+    private NodeStatusWebSocketHandler webSocketHandler;
+
+
     @Override
     public void transition(NodeInstance nodeInstance, String event) {
         switch (nodeInstance.getStatus()) {
             case pending:
                 if ("running".equals(event)) {
                     nodeInstance.setStatus(NodeInstance.NodeInstanceStatus.running);
+                    webSocketHandler.sendNodeStatusUpdate(nodeInstance.getId(), nodeInstance.getNodeId(), NodeInstance.NodeInstanceStatus.running.toString());
                     nodeInstance.setStartedAt(LocalDateTime.now());
                 } else if ("waiting".equals(event)) {
                     nodeInstance.setStatus(NodeInstance.NodeInstanceStatus.waiting);
---
+++ src/main/java/com/lowcode/workflow/runner/graph/websocket/handler/NodeStatusWebSocketHandler.java
@@ -0,0 +1,57 @@
+package com.lowcode.workflow.runner.graph.websocket.handler;
+
+import com.lowcode.workflow.runner.graph.exception.custom.CustomException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Slf4j
+@Component
+public class NodeStatusWebSocketHandler extends TextWebSocketHandler {
+
+    private static final List<WebSocketSession> sessions = new ArrayList<>();
+
+    /**
+     * 当一个连接建立时, 调用该方法, 加入到会话列表中
+     * @param session 新建立的会话
+     * @throws Exception 连接建立失败时抛出
+     */
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+        log.info("a new session: {}, established", session);
+    }
+
+    /**
+     * 处理接收的文本消息
+     * @param session 会话
+     * @param message 文本消息
+     * @throws Exception 处理消息失败时抛出
+     */
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        // 处理接收的消息
+        String payload = message.getPayload();
+        log.info("receive the message: {}", payload);
+    }
+
+    public void sendNodeStatusUpdate(String nodeInstanceId, String nodeId, String status) {
+        log.info("send the node status update to the node: {}, status: {}", nodeInstanceId, status);
+        WebSocketMessage<String> nodeStatusUpdateMessage = new TextMessage(String.format("{\"nodeInstanceId\":\"%s\",\"nodeId\":\"%s\",\"status\":\"%s\"}", nodeInstanceId, nodeId, status));
+        sessions.stream().filter(WebSocketSession::isOpen).forEach(session -> {
+            try {
+                session.sendMessage(nodeStatusUpdateMessage);
+            } catch (IOException e) {
+                throw new CustomException(500, "发送节点状态更新消息失败");
+            }
+        });
+    }
+}
